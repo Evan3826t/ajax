1617ef4fcf004fa27599488d0f3eefb999b81483
Evan3826t <55522290+Evan3826t@users.noreply.github.com>
2019-11-11 16:38:31 +0800

    20191111

--- app2/api/query.php
+++ app2/api/query.php
@@ -1,5 +1,24 @@
 <?php
-//處理查詢資料的請求
+// 處理查詢資料的請求
+$psn="mysql:host=localhost;charset=utf8;dbname=ajax";
+$pdo=new PDO($psn,"root","123");
+// 撈出班級
+
+//  sprintf 數字轉字串
+$class = '1' . sprintf("%02d",$_GET["class"]);
+
+$sql = "SELECT * FROM `students` WHERE substring(`class_num`,1,3)='$class'";
+$row=$pdo->query($sql)->fetchAll();
+
+foreach($row as $r){
+    echo "<tr>";
+    echo "<td>" . mb_substr($r['class_num'],3,2,"utf8") . "</td>";    
+    echo "<td>" . $r['name'] . "</td>";
+    echo "<td>" . $r['dept'] . "</td>";
+    echo "<td>" . $r['birthday'] . "</td>";
+    echo "<td>" . $r['uni_id'] . "</td>";
+    echo "</tr>";
 
 
+}
 ?>

a43d3bad47bbf51c908a6d6696db94ab904a95da
Evan3826t <evan3826t@gmail.com>
2019-11-18 14:26:52 +0800

    20191118
    新增：刪除資料、隱藏滾輪、視窗高度　in app2

---
+++ app2/api/base.php
@@ -0,0 +1,5 @@
+
+<?php
+$psn="mysql:host=localhost;charset=utf8;dbname=ajax";
+$pdo=new PDO($psn,"root","123");
+?>
--- app2/api/query.php
+++ app2/api/query.php
@@ -1,9 +1,7 @@
 <?php
 // 處理查詢資料的請求
-$psn="mysql:host=localhost;charset=utf8;dbname=ajax";
-$pdo=new PDO($psn,"root","123");
 // 撈出班級
-
+include_once("base.php");
 //  sprintf 數字轉字串
 $class = '1' . sprintf("%02d",$_GET["class"]);
 
@@ -17,6 +15,10 @@ foreach($row as $r){
     echo "<td>" . $r['dept'] . "</td>";
     echo "<td>" . $r['birthday'] . "</td>";
     echo "<td>" . $r['uni_id'] . "</td>";
+    // 自創屬性 data-*  把 uid 放入其中
+    echo "<td><button class='edit-user' data-edit='".$r['uni_id']."'>編輯</button></td>";
+    echo "<td><button class='del-user' data-del='".$r['uni_id']."'>刪除</button></td>";
+    
     echo "</tr>";
 
 
